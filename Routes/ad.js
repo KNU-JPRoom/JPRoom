ff05c9538280a26dc89eeca51e503f632cf37a85
KNU-JPRoom <couplevaccine@naver.com>
2020-09-30 16:20:38 +0900

    200930 오류 수정 및 파일 추가

--- Routes/ad.js
+++ Routes/ad.js
@@ -3,15 +3,22 @@ module.exports = function(app,session,db){
     const express = require('express');
     const router = express.Router();
     const ejs = require('ejs');
-    const request = require('./ad_Request');
+
+    const requestE = require('./ad_RequestEnroll');
+    const requestB = require('./ad_RequestBuy');
 
     router.get('/Description',function(req,res,next){
       res.render('User/Admin/ad_Description',{'app':app,'session':session,'db':db});
     });
 
-    router.get('/Request',function(req,res,next){
-      request.requestWH(req,res,app,db);
-      res.render('User/Admin/ad_Request',{'app':app,'session':session,'db':db});
+    router.get('/RequestEnroll',function(req,res,next){
+      requestE.requestWH(req,res,app,db);
+      res.render('User/Admin/ad_RequestEnroll',{'app':app,'session':session,'db':db});
+    });
+
+    router.get('/RequestBuy',function(req,res,next){
+      requestB.requestBuy(req,res,app,db);
+      res.render('User/Admin/ad_RequestBuy',{'app':app,'session':session,'db':db});
     });
 
     router.get('/Question',function(req,res,next){
