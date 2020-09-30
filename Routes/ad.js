7aea35acc796f0abe01dd8cbd0e98f8c96e66ad7
KNU-JPRoom <couplevaccine@naver.com>
2020-09-30 16:09:20 +0900

    200930 에러 수정

--- Routes/ad.js
+++ Routes/ad.js
@@ -3,7 +3,6 @@ module.exports = function(app,session,db){
     const express = require('express');
     const router = express.Router();
     const ejs = require('ejs');
-
     const request = require('./ad_Request');
 
     router.get('/Description',function(req,res,next){
@@ -11,8 +10,6 @@ module.exports = function(app,session,db){
     });
 
     router.get('/Request',function(req,res,next){
-
-    res.render('User/Admin/ad_Request',{'app':app,'session':session,'db':db});
       request.requestWH(req,res,app,db);
       res.render('User/Admin/ad_Request',{'app':app,'session':session,'db':db});
     });
