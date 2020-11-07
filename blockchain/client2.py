84ecec8131868288a30d8591cc2a27af59d48d5c
IOrez <h.5363@daum.net>
2020-11-07 18:42:25 +0900

    블록체인 프로토타입

--- blockchain/client2.py
+++ blockchain/client2.py
@@ -1,91 +1,130 @@
 import socket
+import threading
 from threading import Thread,Lock
 import sys
 import pickle  # socket.send에서 list 보내기 위한 module
 from blockchain import *
+import json
+import hashlib
+import time
 
-HOST = 'localhost'
+bcLock = threading.Lock();
+bqLock = threading.Lock();
+rqLock = threading.Lock();
+
+HOST = '13.125.153.38'
 PORT = 7777
-state = '0'
 #q = queue.Queue()
 
-blockchain = Blockchain()
-chain = blockchain.chain
-
+blockChain = []
+recordQueue = []
+blockQueue = []
 def rcvMsg(sock):
+    global blockQueue
+    global recordQueue
+
     while True:
         try:
-            data = sock.recv(1024)
-            if data.decode() == 'send':
-                try:
-                    m = sock.recv(1024)
-                    print(json.loads(m))
-                    if not data:
-                        print(empty)
-                except:
-                    pass
-            print(data.decode())
-            if not data:
-                break
-        except:
-            pass
+            data = sock.recv(1000)
+            MSG = pickle.loads(data)
+            print(MSG)
+            if MSG['MSGTYPE'] == 'INIT_BLOCK':
+                bcLock.acquire()
+                for i in MSG['data']:
+                    blockChain.append(MSG['data'][i])
+                blockChain.reverse()
+                bcLock.release()
+                sock.send(pickle.dumps({'MSGTYPE':'REQ_OK','ID':'fad'}));
+            elif MSG['MSGTYPE'] == 'BLOCK':
+                    bqLock.acquire()
+                    blockQueue.append(MSG['data'])
+                    bqLock.release()
+            elif MSG['MSGTYPE'] == 'RECORD':
+                    rqLock.acquire()
+                    recordQueue.append(MSG['data'])
+                    rqLock.release()
+        except Exception as ex:
+            print('에러 발생', ex)
+
 
 def runChat():
+    global blockQueue
+    global recordQueue
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:     #socket 생성
         sock.connect((HOST,PORT))
-        t = Thread(target=rcvMsg, args=(sock,))
+
+        id = None
+        rMsg = sock.recv(1024)
+        rMsg = rMsg.decode()
+
+        print(rMsg,end='')
+
+        id = input()
+        sock.send(id.encode())
+        block2 = {
+                'hash': 0,
+                'updatable':True,
+                'data':
+                {
+                    'index':0,
+                    'timestamp': 0,
+                    'transaction': 0,
+                    'proof': 0,
+                    'difficulty': 0,
+                    'previous_hash': 0
+                }
+             }
+        MSG = {'MSGTYPE':'record','ID':'Master','data':{'index': 1, 'timestamp' : 0, 'transaction': 'makeTrans', 'proof': 0, 'difficulty' : 2,  'previous_hash': 'DUHFHI14GUNEN6fuWDAHFH)'}}
+
+        t1 = Thread(target=rcvMsg, args=(sock,))
         #pre_chain = q.get(chain)
         #chain = pre_chain
-        t.daemon = True   #Thread 클래스에서 daemon 속성은 서브쓰레드가 데몬 쓰레드인지 아닌지를 지정하는 것인데, 데몬 쓰레드란 백그라운드에서 실행되는 쓰레드로 메인 쓰레드가 종료되면 즉시 종료되는 쓰레드이다. 반면 데몬 쓰레드가 아니면 해당 서브쓰레드는 메인 쓰레드가 종료할 지라도 자신의 작업이 끝날 때까지 계속 실행된다.
-        t.start()
+        t1.daemon = True   #Thread 클래스에서 daemon 속성은 서브쓰레드가 데몬 쓰레드인지 아닌지를 지정하는 것인데, 데몬 쓰레드란 백그라운드에서 실행되는 쓰레드로 메인 쓰레드가 종료되면 즉시 종료되는 쓰레드이다. 반면 데몬 쓰레드가 아니면 해당 서브쓰레드는 메인 쓰레드가 종료할 지라도 자신의 작업이 끝날 때까지 계속 실행된다.
+        t1.start()
+
         while True:
-            msg = input()
-            if msg == '/quit':
-                sock.send(msg.encode())
-                break
-            if msg == 'chain':
-                print(blockchain)       #blockchain객체의 주소를 출력함.
-                # queue 로 동기화(;)
-                #pre_chain = blockchain.chain
-                #q.put(pre_chain)
-                #chain = q.get()
-                #------
-                #q.put(chain)
-                #chain = blockchain.chain
-                data = pickle.dumps(chain)
-                sock.send(data)
-                continue
-            if msg == 'mine':                       #추후 입력을 받지않아도 일정 수의 contarct data가 쌓이면 mining하게끔 해주어야함.
-                last_block = blockchain.last_block
-                last_proof = last_block['proof']    #block 에 저장되어있는 json내부의 proof property를 저장. proof property에는 magic number(최초 3개의 해쉬값 000을 만족시키기 위해 산출된 숫자)
-                previous_hash = blockchain.hash(last_block)
-                proof = blockchain.pow(last_proof, previous_hash)
-                block = blockchain.new_block(proof, previous_hash)
-                #queue 에 block data 추가
-                #q.put(block)
-                response = {
-                'message' : 'new block found',
-                'index' : block['index'],
-                'transactions' : block['transactions'],
-                'proof' : block['proof'],
-                'previous_hash' : block['previous_hash'],
-                'hash' : blockchain.hash(block)
-                }
-                blockchain.block_hash.append(blockchain.hash(block))
-                data = pickle.dumps(response)
-                sock.send(data)
-                continue
-            if(msg == 'ready'):
-                data = 'ready'
-                data = pickle.dumps(data)
-                sock.send(data)
 
-                continue
-#            if(state == '1'):        #준비가 된 상태이면
-#                date = pickle.dumps(state)
-#                sock.send(data)
-#                continue
+            bqLock.acquire()
+#            print('bqLock acquire')
+            block = None
+            if len(blockQueue)!=0:
+                block = blockQueue[0]        #queue에서 아무것도 없으면 무엇을 반환하나?
+                blockQueue = blockQueue[1:]
+            bqLock.release()
 
-            sock.send(msg.encode())
+            if block != None:
+                bcLock.acquire()
+                blockChain.append(block)
+                bcLock.release()
+                continue
+            else:
+                record = None
+                rqLock.acquire()
+                if len(recordQueue)!=0:
+                    record = recordQueue[0]
+                    recordQueue = recordQueue[1:]
+                    print(record)
+                rqLock.release()
+                if record == None:
+                    continue
+
+                bcLock.acquire()
+                if len(blockChain)!=0 and blockChain[-1]['data']['index'] >= record['index']:   #record에 있는 블록보다 체인에 존재하는 블록이 최신이면 버림.
+                    continue
+                else:           #더 최신정보이면 mining
+                    proof = 0
+                    while True:
+                        record['proof'] = proof
+                        blockDstring = json.dumps(record).encode()
+                        guess = hashlib.sha256(blockDstring).hexdigest()
+                        if guess[:record['difficulty']] == "0"*record['difficulty']:
+                            print(guess)
+                            break
+                        else:
+                            proof = proof + 1
+                    print(record['proof'])
+                    sock.send(pickle.dumps({'MSGTYPE':'REQ_MAKEBLOCK','ID': 'piora', 'index' : record['index'], 'proof': proof}))
+                bcLock.release()
 
 runChat()
--- blockchain/server2.py
+++ blockchain/server2.py
@@ -3,141 +3,203 @@ import threading
 import pickle
 import sys
 import json
-import blockchain
+from time import time
+import datetime
+import hashlib
+
 from json import loads
-storage = []
-["provider_id and buyer_id are contracted eachother ",]
+
 HOST = '127.0.0.1'
 PORT = 7777
 #q = queue.Queue()                #연속적으로 들어올 condata를 저정하기위한 큐
-contractData = {}
+#contractData = {}
 
-contractData['1'] = '{"transaction": "sena->russian", "amount": 30}'
+#contractData['1'] = '{"transaction": "sena->russian", "amount": 30}'
 
 lock = threading.Lock() # syncronized 동기화 진행하는 스레드 생성
 
-class UserManager: # 사용자관리 및 채팅 메세지 전송을 담당하는 클래스
+class UserManager(): # 사용자관리 및 채팅 메세지 전송을 담당하는 클래스
                    #  채팅 서버로 입장한 사용자의 등록
                    #  채팅을 종료하는 사용자의 퇴장 관리
                    #  사용자가 입장하고 퇴장하는 관리
                    #  사용자가 입력한 메세지를 채팅 서버에 접속한 모두에게 전송(broadcast)
-
    def __init__(self):
-       self.users = {} # 사용자의 등록 정보를 담을 사전 {사용자 이름:(소켓,주소),...}
-       self.readyState = {} #각 client의 준비상태를 저장하는 dictionnary {사용자이름:(state)}
+      self.users = {} # 사용자의 등록 정보를 담을 사전 {사용자 이름:(소켓,주소),...}
+      self.readyState = {} #각 client의 준비상태를 저장하는 dictionnary {사용자이름:(state)}
 
    def addUser(self, username, conn, addr): # 사용자 ID를 self.users에 추가하는 함수
       if username in self.users: # 이미 등록된 사용자라면
          conn.send('이미 등록된 노드입니다.\n'.encode())
          return None
 
-      # 새로운 사용자를 등록함
+          # 새로운 사용자를 등록함
       lock.acquire() # 스레드 동기화를 막기위한 락
       self.users[username] = (conn, addr)
       lock.release() # 업데이트 후 락 해제
-
-      self.sendMessageToAll('[%s]님이 입장했습니다.' %username)
       print('+++ 노드 참여자 수 [%d]' %len(self.users))
-
       return username
 
-   def setUserState(self, username, state):
+#   def setUserState(self, username, state):
+#       lock.acquire()
+##       lock.release()
+
+#       print('set state about [%s]' %username)
+#       return
+
+   def removeUser(self, username):
+       if username not in self.users:
+          return
+
        lock.acquire()
-       self.readyState[username] = (state)
+       del self.users[username]
        lock.release()
-
-       print('set state about [%s]' %username)
-       return
-
-   def removeUser(self, username): #사용자를 제거하는 함수
-      if username not in self.users:
-         return
-
-      lock.acquire()
-      del self.users[username]
-      lock.release()
-
-      self.sendMessageToAll('[%s]님이 퇴장했습니다.' %username)
-      print('--- 노드 참여자 수 [%d]' %len(self.users))
+       print('--- 노드 참여자 수 [%d]' %len(self.users))
 
 
-   def messageHandler(self, username, msg):     # 전송한 msg를 처리하는 부분
-      if msg == '/quit':                        # 보낸 메세지가 'quit'이면
-         self.removeUser(username)
-         return -1
-      elif msg == 'ready':                          #client측에서 준비완료된 상태라면.
-         self.setUserState(username, 1)
-         if self.isReady() == 0:
-            self.sendMessageToAll('send')
-            self.sendConDataToAll(contractData['1'])
-      else:
-         self.sendMessageToAll('[%s] %s' %(username, msg))    #input이 'quit'가 아니라면 broadcast
-         return
-
-      return
-
-   def setReadyState(self, username, msg):
-      if msg == 'ready':
-         self.readyState[username] = (1)
-
-      self.sendMessageToAll('ready state : [%d]' %(self.readyState[username]))
-
-   def isReady(self):         #서버에서 계속돌면서 체킹하려고함.
-      flag = 0
-
-      for state in self.readyState.values():
-             if state == 1:
-                 continue
-             else:
-                 flag = -1
-      return flag
+   def messageHandler(self, username, msg):
+       if msg == '/quit':                        # 보낸 메세지가 'quit'이면
+          self.removeUser(username)
+          return -1
+       else:
+          self.sendMessageToAll('[%s] %s' %(username, msg))    #input이 'quit'가 아니라면 broadcast
+          return
 
 
-   def sendMessageToAll(self, msg): #BroadCast 부분. 추후 변경필요할듯.
-      for conn, addr in self.users.values():
-             conn.send(msg.encode())
+   def sendMessageToAll(self, msg):
+       for conn, addr in self.users.values():
+          conn.send(pickle.dumps(msg))
 
    def sendConDataToAll(self, msg):
-      for conn, addr in self.users.values():
-             conn.send(json.dumps(msg).encode())       #json 형태의 string을 encode해서 각 client에게 전송.
+       for conn, addr in self.users.values():
+          conn.send(pickle.dumps(msg))       #json 형태의 string을 encode해서 각 client에게 전송.
 
-#   def sendDataToClient(self, msg):
-#           self.sendMessageToAll(self, 'John -> Jenny : 15')
+   def sendDataToClient(self, msg):
+       self.sendMessageToAll(self, 'John -> Jenny : 15')
 
 
 
 class MyTcpHandler(socketserver.BaseRequestHandler):
-   userman = UserManager()
+    LIMIT_QUNTITY = 1
+    difficulty = 2
+    userman = UserManager()
+    conBuff = ["jax send 50$ to piora", "yasuo send 30$ to shen", "ramus send 10$ to anivia"]
+    blockindex = 0
+    previous_hash = 0
+    blockChain = [
+        {
+            'hash':'',
+            'updatable':True,
+            'data':
+            {
+                'index':blockindex,
+                'timestamp': time(),
+                'transaction': 'start',
+                'proof': 0,
+                'difficulty': difficulty,
+                'previous_hash': previous_hash
+             }
+        }
+    ]
+#    print(blockChain)
+    blockDstring = json.dumps(blockChain[0]['data'], sort_keys=True).encode()
+    blockChain[0]['hash'] = hashlib.sha256(blockDstring).hexdigest()
+    previous_hash = blockChain[0]['hash']
+#    print(blockChain)
+    MSG = {'MSGTYPE':'init','ID':'Master','completable': False,
+           'data':{'index': blockindex, 'timestamp' : 0, 'transaction': conBuff, 'proof': 0, 'difficulty' : difficulty,  'previous_hash': previous_hash}}
+    blockindex = blockindex + 1;
 
-   def handle(self):                                    #클라이언트에서 요청이 들어오면 handle이 호출됨.
+    # block1 = {
+    #             'hash': hashlib.sha256(json.dumps(conBuff[0]).encode()).hexdigest(),
+    #             'updatable':True,
+    #             'data':
+    #             {
+    #                 'index':blockindex,
+    #                 'timestamp': time(),
+    #                 'transaction': conBuff[0],
+    #                 'proof': 0,
+    #                 'difficulty': difficulty,
+    #                 'previous_hash': blockChain[0]['hash']
+    #             }
+    #          }
+    # blockindex = blockindex + 1
+    # block2 = {
+    #             'hash': hashlib.sha256(json.dumps(conBuff[1]).encode()).hexdigest(),
+    #             'updatable':True,
+    #             'data':
+    #             {
+    #                 'index':blockindex,
+    #                 'timestamp': time(),
+    #                 'transaction': conBuff[1],
+    #                 'proof': 0,
+    #                 'difficulty': difficulty,
+    #                 'previous_hash': blockChain[1]['hash']
+    #             }
+    #          }
+
+    def handle(self):                                    #클라이언트에서 요청이 들어오면 handle이 호출됨.
       print('[%s] 연결됨' %self.client_address[0])       # 클라이언트가 접속시 클라이언트 주소 출력
       try:
           username = self.registerUsername()
-          self.userman.setUserState(username, 0)
+#          self.userman.setUserState(username, 0)
+          dic = {i:obj for i, obj in enumerate(self.blockChain[::-1])}        #가장 최근(가장 끝의) 블록에 번호를 매겨 역순으로 딕셔너라에 저장 후 노드에게 전송.
+
+          self.userman.users[username][0].send(pickle.dumps({'MSGTYPE': 'INIT_BLOCK', 'data': dic}))
           while True:
-             pre_msg = self.request.recv(16394)     #client에서 전송한 정보를 받음.
-
-             #if type(pre_msg) == bytes:
-             msg = pickle.loads(pre_msg)            #json형태의 msg를 역직렬화함.
-             while pre_msg:
-                 print(msg)
-                 if self.userman.messageHandler(username,msg) == -1:    #/quit 입력시 return -1
-                     self.request.close()
-                     break
-
-                 pre_msg = self.request.recv(1024)   #self.request는 client에 연결된 TCP socket pre_msg에는 추가로 입력되는 input value에 대한 json데이터가 들어올것임.
-                 msg = pickle.loads(pre_msg)
-
-
-         #else:
-        #     while pre_msg:
-        #         if pre_msg == 'mine'.decode():
-        #             print('mining'.encode())
-        #             print(pre_msg.decode())
-        #             if self.userman.messageHandler(username, pre_msg.decode()) == -1:
-        #                 self.request.close()
-        #                 break
-        #             pre_msg = self.request.recv(1024)
+#             tmpMsg = self.MSG
+#             tmpMsg['okSign'] = True
+#             self.userman.users[username][0].send(pickle.dumps(tmpMsg))
+             buf = self.userman.users[username][0].recv(1000)     #client에서 전송한 정보를 받음.(블록)
+             revMsg = pickle.loads(buf)
+             print(revMsg)
+             if revMsg['ID'] == "WEBSERVER":
+                 self.conBuff.append(revMsg['data'])
+                 if len(self.conBuff) >= self.LIMIT_QUNTITY and self.blockChain[(self.blockindex)-1]['updatable']== True:
+                     self.MSG['data']['transaction']=self.conBuff[:self.LIMIT_QUNTITY]
+                     self.conBuff = self.conBuff[self.LIMIT_QUNTITY:]
+                     timestamp = time()
+                     self.MSG['data']['timestamp'] = timestamp
+                     self.blockChain.append({
+                              'updatable':False,
+                              'data':
+                              {
+                                'index':self.blockindex,
+                                'timestamp': timestamp,
+                                'transaction': self.MSG['transaction'],
+                                'proof': 0,
+                                'difficulty': self.difficulty,
+                                'previous_hash': self.previous_hash
+                               }
+                            })
+                     self.MSG['MSGTYPE'] = 'RECORD'
+                     self.MSG['ID']='Master'
+                     userman.self.sendConDataToAll(self.MSG)
+                     self.blockindex = (self.blockindex) + 1
+             else:
+                 if revMsg['MSGTYPE'] == 'REQ_MAKEBLOCK':
+                     index = revMsg['index']
+                     proof = revMsg['proof']
+                     cpDic = self.blockChain[index]['data'].copy()
+                     cpDic['proof'] = proof
+                     guess = json.dumps(cpDic).encode()
+                     guess_hash = hashlib.sha256(guess).hexdigest()     #node에서 전송한 블록이 유효한지 검사.
+                     print(guess_hash)
+                     if guess_hash[:self.difficulty]== "0"*self.difficulty:       #유효하다면
+                         self.blockChain[index]['hash']=guess_hash
+                         self.blockChain[index]['updatable']=True
+                         self.previous_hash = guess_hash
+                         print(self.blockChain[index])
+                         MSG = {'MSGTYPE':'BLOCK','ID':'Master','data':self.blockChain[index]}
+                         self.userman.sendConDataToAll(MSG)
+                     else:
+                         MSG = {'MSGTYPE': 'RECORD','ID':'Master','data':self.blockChain[index]['data'] }
+                         self.userman.users[username][0].send(pickle.dumps(MSG))
+                 elif revMsg['MSGTYPE'] == 'REQ_OK':
+                     MSG = {'MSGTYPE':'RECORD','ID':'Master',
+                                   'data':{'index': self.blockindex, 'timestamp' : 0, 'transaction': 'dkslqwkle;qwek', 'proof': 0, 'difficulty' : self.difficulty,  'previous_hash': self.previous_hash}}
+                     self.userman.users[username][0].send(pickle.dumps(MSG))
+                     self.blockChain.append({'hash': 0, 'updatable': False, 'data': MSG['data']})
+                     self.blockindex = self.blockindex + 1
 
       except Exception as e:
          print(e)
@@ -145,15 +207,18 @@ class MyTcpHandler(socketserver.BaseRequestHandler):
       print('[%s] 접속종료' %self.client_address[0])
       self.userman.removeUser(username)
 
-   def registerUsername(self):
+    def registerUsername(self):
       while True:
-         self.request.send(json.dumps('ID :').encode())
+         self.request.send("ID: ".encode())
+         lock.acquire()
          username = self.request.recv(1024) #유저가 ID입력시 receive
+#         print(username)
          username = username.decode().strip()   #decoding input data
+         lock.release()
          if self.userman.addUser(username, self.request, self.client_address):  #self.request -> conn
             return username
 
-   def checkRdState(self):
+    def checkRdState(self):
       while True:
          if self.userman.isReady() == 0:
              self.userman.sendMessageToAll('all ready')
@@ -170,7 +235,7 @@ def runServer():
 
    try:
       server = ChatingServer((HOST, PORT), MyTcpHandler)
-      server.serve_forever()        #접속요청이 오면 수락한 뒤 BaseRequestHandler 객체의 handle()메소드 호출.
+      server.serve_forever()
    except KeyboardInterrupt:
       print('--- blockchain network를 종료합니다.')
       server.shutdown()
